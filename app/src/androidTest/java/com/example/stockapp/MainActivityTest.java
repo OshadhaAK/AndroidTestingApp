a693846d9fedb46cd2d78b013682c1d94c288746
Oshadha <oshadhak@hsenidmobile.com>
2019-08-09 12:15:28 +0530

    testing full flow

--- app/src/androidTest/java/com/example/stockapp/MainActivityTest.java
+++ app/src/androidTest/java/com/example/stockapp/MainActivityTest.java
@@ -1,5 +1,6 @@
 package com.example.stockapp;
 
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
@@ -15,9 +16,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.core.IsNot.not;
 
 public class MainActivityTest {
 
@@ -78,4 +76,70 @@ public class MainActivityTest {
         onView(withId(R.id.SignIn)).perform(click());
         onView(withId(R.id.password)).check(matches(hasErrorText("Cannot be empty!")));
     }
+
+    @Test
+    public void testSignInFlow(){
+        // fill fields
+        onView(withId(R.id.userName)).perform(typeText("admin"),closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText("admin"),closeSoftKeyboard());
+        // click button
+        onView(withId(R.id.SignIn)).perform(click());
+
+        // check the home page is visible after click
+        onView(withId(R.id.productList)).check(matches(isDisplayed()));
+
+        //check the view buton
+        onView(withId(R.id.productList)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        //check addtocart button exists
+        onView(withId(R.id.addToCart)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.addToCart)).perform(click());
+
+        //check checkout button exists
+        onView(withId(R.id.btnCheckout)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.btnCheckout)).perform(click());
+
+        //check transaction succesfull activity
+        onView(withId(R.id.successmsg)).check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void testSignUpFlow(){
+
+        onView(withId(R.id.SignUp)).perform(click());
+        // fill fields
+        onView(withId(R.id.user_name)).perform(typeText("Oshadha"),closeSoftKeyboard());
+        onView(withId(R.id.emailAddress)).perform(typeText("oshadhak@hsenidmobile.com"),closeSoftKeyboard());
+        onView(withId(R.id.phoneNum)).perform(typeText("+94722256971"),closeSoftKeyboard());
+        onView(withId(R.id.addPassword)).perform(typeText("admin"),closeSoftKeyboard());
+        // check signup button
+        onView(withId(R.id.btnSignup)).perform(click());
+
+        //test welcome toast message
+        onView(withText(R.string.welcomeMessage)).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
+
+        // check the home page is visible after click
+        onView(withId(R.id.productList)).check(matches(isDisplayed()));
+
+        //check the view buton
+        onView(withId(R.id.productList)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        //check addtocart button exists
+        onView(withId(R.id.addToCart)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.addToCart)).perform(click());
+
+        //check checkout button exists
+        onView(withId(R.id.btnCheckout)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.btnCheckout)).perform(click());
+
+        //check transaction succesfull activity
+        onView(withId(R.id.successmsg)).check(matches(isDisplayed()));
+
+    }
+
 }

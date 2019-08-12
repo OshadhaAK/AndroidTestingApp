31d134b5016152ec5d3a48475b47acd652384ccd
Oshadha <oshadhak@hsenidmobile.com>
2019-08-12 14:06:36 +0530

    API integration

--- app/src/main/java/com/example/stockapp/HomeActivity.java
+++ app/src/main/java/com/example/stockapp/HomeActivity.java
@@ -1,14 +1,22 @@
 package com.example.stockapp;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,13 +27,16 @@ public class HomeActivity extends AppCompatActivity {
     private RecyclerView productList;
     private List<String> list;
     private productAdapter adapter;
+    private RequestQueue requestQueue;
 
+    private String url = "http://172.16.2.139:8081/movies/get-all";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        requestQueue = Volley.newRequestQueue(this);
 
         productList = (RecyclerView)findViewById(R.id.productList);
 
@@ -33,12 +44,47 @@ public class HomeActivity extends AppCompatActivity {
         productList.setLayoutManager(new LinearLayoutManager(this));
 
         list = new ArrayList<>();
-        for (int i=1;i<=20;i++){
-            list.add("Product " + i);
-        }
+        getNames();
+
 
         adapter = new productAdapter(this,list);
         productList.setAdapter(adapter);
+
+    }
+
+    private void getNames(){
+        JsonArrayRequest arrayRequest = new JsonArrayRequest(Request.Method.GET, this.url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        if (response.length() > 0) {
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    JSONObject jsonObject = response.getJSONObject(i);
+                                    String name = jsonObject.get("name").toString();
+                                    list.add(name);
+                                } catch (JSONException e) {
+                                    Log.e("Volley", "Invalid JSON Object.");
+                                }
+                            }
+                        } else {
+
+                        }
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Volley", error.toString());
+                    }
+                }
+
+        );
+
+        requestQueue.add(arrayRequest);
+
+
 
     }
 }
--- app/src/main/java/com/example/stockapp/SuccessActivity.java
+++ app/src/main/java/com/example/stockapp/SuccessActivity.java
@@ -1,14 +1,19 @@
 package com.example.stockapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SuccessActivity extends AppCompatActivity {
 
+    private TextView successMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
+
+        successMessage = (TextView)findViewById(R.id.successmsg);
+        successMessage.setText("Successfully Completed!");
     }
 }
